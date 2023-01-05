c953c5e265cb6a9ad34dc8015323e13a56a00d4d
jiji20100 <jihadrifi909@gmail.com>
2023-01-05 19:58:00 +0100

    Application - mise en place du hilt dans le ViewModel

--- app/src/main/java/com/example/daricom/viewModel/EstimmoViewModel.kt
+++ app/src/main/java/com/example/daricom/viewModel/EstimmoViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.daricom.viewModel
 
-class EstimmoViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.daricom.backend.EstimmoUtil
+import com.example.daricom.data.EstimmoResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+const val STATE_KEY_RESULT = "state_key_result"
+
+@HiltViewModel
+class EstimmoViewModel @Inject constructor(
+    state: SavedStateHandle,
+    private val estimmoUtil: EstimmoUtil
+): ViewModel() {
+
+    private val _estimmoResult: MutableLiveData<EstimmoResult> = state.getLiveData(STATE_KEY_RESULT, EstimmoResult.Empty)
+    val estimmoResult: LiveData<EstimmoResult> = _estimmoResult
+
 }

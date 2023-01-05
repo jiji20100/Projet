3c1188a065751a38120d4e722ae933fa23bf500e
jiji20100 <jihadrifi909@gmail.com>
2023-01-05 19:57:42 +0100

    Application - mise en place du hilt

--- app/src/main/java/com/example/daricom/view/HomeFragment.kt
+++ app/src/main/java/com/example/daricom/view/HomeFragment.kt
@@ -1,4 +1,32 @@
 package com.example.daricom.view
 
-class HomeFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.daricom.R
+import com.example.daricom.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeFragment : Fragment() {
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.homeButton.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_estimmoFragment)
+        }
+    }
+
 }
--- app/src/main/java/com/example/daricom/view/MainActivity.kt
+++ app/src/main/java/com/example/daricom/view/MainActivity.kt
@@ -2,11 +2,15 @@ package com.example.daricom.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.daricom.R
+import com.example.daricom.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
 }

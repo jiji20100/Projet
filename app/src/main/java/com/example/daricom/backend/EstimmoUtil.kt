971ed3dde74af2a745341f55f954327c925711e6
Jihad RIFI <93480561+jiji20100@users.noreply.github.com>
2023-01-06 19:16:26 +0100

    Update EstimmoUtil.kt

--- app/src/main/java/com/example/daricom/backend/EstimmoUtil.kt
+++ app/src/main/java/com/example/daricom/backend/EstimmoUtil.kt
@@ -1,5 +1,13 @@
 package com.example.daricom.backend
-
+    /** 
+     * Returns estimated price of a property
+     * @param [property] type of property
+     * @param [surface] surface of the property
+     * @param [section] section of the property
+     * @param [piece] number of rooms of the property
+     * return [price] estimated price of the property
+     */
+     
 interface EstimmoUtil {
 
     fun estimmo(proprety : String, section : String, terrain : Float?, surface : Float, piece : Int): Float
